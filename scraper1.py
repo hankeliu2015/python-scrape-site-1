dcba402de4e38266f442b80733653b430ed9569a
hanliu12 <hanliu12@publicisgroupe.net>
2022-01-19 14:49:52 -0500

    test different parsers

--- scraper1.py
+++ scraper1.py
@@ -5,9 +5,13 @@ from bs4 import BeautifulSoup
 req = requests.get('https://www.youtube.com/watch?v=QwZT7T-TXT0')
 
 # soup = BeautifulSoup(req.content, "html.parser")
-soup = BeautifulSoup(req.content, "html5lib")
+soup = BeautifulSoup(req.content, "lxml")
+# soup = BeautifulSoup(req.content, "html5lib")
 
-with open('page_output1.txt', mode='w') as file_object:
+# with open('page_output_html5lib.txt', mode='w') as file_object:
+#     print(soup.prettify, file=file_object)
+
+with open('page_output_lxml.txt', mode='w') as file_object:
     print(soup.prettify, file=file_object)
 
 # print(soup.prettify)
