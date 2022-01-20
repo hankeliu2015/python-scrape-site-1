324b34ccc8bb0f0f110f6cb4da30a69cb1c2f8ce
hanliu12 <hanliu12@publicisgroupe.net>
2022-01-19 23:06:39 -0500

    set up evnironment varible for youtube data api key.

--- scraper1.py
+++ scraper1.py
@@ -1,5 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+from apiclient.discovery import build
+from decouple import config
+
+YOUTUBE_API_SERVICE_NAME = config('YOUTUBE_API_SERVICE_NAME')
+YOUTUBE_API_VERSION = config('YOUTUBE_API_VERSION')
+DEVELOPER_KEY = config('DEVELOPER_KEY')
+
+# creating Youtube Resource Object
+youtube_object = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+                       developerKey=DEVELOPER_KEY)
+
+print(youtube_object)
 
 # req = requests.get('https://www.geeksforgeeks.org')
 req = requests.get('https://www.youtube.com/watch?v=QwZT7T-TXT0')
@@ -11,8 +23,8 @@ soup = BeautifulSoup(req.content, "lxml")
 # with open('page_output_html5lib.txt', mode='w') as file_object:
 #     print(soup.prettify, file=file_object)
 
-with open('page_output_lxml.txt', mode='w') as file_object:
-    print(soup.prettify, file=file_object)
+# with open('page_output_lxml.txt', mode='w') as file_object:
+#     print(soup.prettify, file=file_object)
 
 # print(soup.prettify)
 
